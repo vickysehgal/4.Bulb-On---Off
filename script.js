515c15dda49bafad82a119d0a34e1a03e5b7e770
vickysehgal <vk1512694@gmail.com>
2023-06-12 13:14:43 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+function bulbOn(){
+    let bulb = document.querySelector(".bulb-off");
+    bulb.setAttribute("src", "./images/bulb_on.png");
+}
+
+function bulbOff(){
+    let bulb = document.querySelector(".bulb-off");
+    bulb.setAttribute("src", "./images/bulb_off.png")
+}
